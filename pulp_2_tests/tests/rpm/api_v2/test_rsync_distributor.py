f0de568edc3c3980cf2eba5977022e8e6cdc62ef
koliveir <koliveir@redhat.com>
2019-09-09 17:15:17 -0400

    Update AddUnitTestCase assertion
    Update AddUnitTestCase as per changes related to #4059.
    
    See: https://pulp.plan.io/issues/4059

--- pulp_2_tests/tests/rpm/api_v2/test_rsync_distributor.py
+++ pulp_2_tests/tests/rpm/api_v2/test_rsync_distributor.py
@@ -37,15 +37,15 @@ files will be laid out as follows::
 
     /home/myuser
     ├── foo
-    │   └── bar
-    │       └── biz
-    │           └── rpm
-    │               ├── 06
-    │               │   └── …
-    │               │       └── dog-4.23-1.noarch.rpm
-    │               ├── 09
-    │               │   └── …
-    │               ┆       └── crow-0.8-1.noarch.rpm
+    │   └── bar
+    │       └── biz
+    │           └── rpm
+    │               ├── 06
+    │               │   └── …
+    │               │       └── dog-4.23-1.noarch.rpm
+    │               ├── 09
+    │               │   └── …
+    │               ┆       └── crow-0.8-1.noarch.rpm
     └── rel-url
         ├── bear-4.1-1.noarch.rpm -> ../foo/bar/biz/rpm/a9/…/bear-4.1-1.noarch…
         ├── camel-0.1-1.noarch.rpm -> ../foo/bar/biz/rpm/92/…/camel-0.1-1.noar…
@@ -652,7 +652,9 @@ class AddUnitTestCase(
     This test case targets:
 
     * `Pulp #2532 <https://pulp.plan.io/issues/2532>`_
+    * `Pulp #4059 <https://pulp.plan.io/issues/4059>`_
     * `Pulp Smash #526 <https://github.com/PulpQE/pulp-smash/issues/526>`_
+
     """
 
     def test_all(self):
@@ -684,7 +686,8 @@ class AddUnitTestCase(
         # Publish with yum and rsync, respectively.
         for key in 'yum_distributor', 'rpm_rsync_distributor':
             publish_repo(cfg, repo, {'id': dists[key]['id']})
-        self.verify_remote_units_path(cfg, dists['rpm_rsync_distributor'], 1)
+        # This test assertion was updated by https://pulp.plan.io/issues/4059
+        self.verify_remote_units_path(cfg, dists['rpm_rsync_distributor'], 2)
 
 
 class PublishTwiceTestCase(
